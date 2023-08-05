1b6e60fee02d1bc7cd697b2c6a9ccb207c122617
GuilhermeAlamino <guilhermedevelopedweb@hotmail.com>
2023-08-05 18:25:25 -0300

    criando as seeds para rodar as fabricas fakes criadas e popular o banco

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(DepartmentSeeder::class);
+        $this->call(EmployeeSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(TaskSeeder::class);
     }
 }
